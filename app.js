0e096aba1ff22dc1bff6da69df71a1e535e4f585
Fahim Afnan <142663426+fahim6855@users.noreply.github.com>
2026-03-07 06:33:28 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,45 @@
+let baseUrl = "";
+let apiKey = "47e8febd32a5972b12bef57865cb08a5";
+let streamUrl = "https://streamex.net"
+
+document.addEventListener("alpine:init", () => {
+  Alpine.data("movieData", () => ({
+    outputMovies: [],
+    savedMovies: [],
+    activeGenre: "popular",
+    searchTitle: "",
+    streamLink : streamUrl,
+    async init() {this.outputMovies = await getPopularMovies()},
+    async showActions(id){
+      this.activeGenre = id;
+      this.outputMovies = await getByGenre(id)
+    },
+    async showSearch(){this.outputMovies = await getBySearch(this.searchTitle);},
+  }));
+});
+
+
+//getPopularMovies();
+async function getPopularMovies() {
+  let resPop = await fetch(
+    `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&page=1`
+  );
+  let dataPop = await resPop.json();
+  return dataPop.results;
+}
+
+//getByGenre("16")
+async function getByGenre(genreId){
+  let resGenre = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=${Number(genreId)}`);
+  let dataGenre = await resGenre.json();
+  return dataGenre.results;
+}
+
+
+async function getBySearch(QueryTitle){
+  let resSearch = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${QueryTitle}`);
+  let dataSearch = await resSearch.json();
+  console.log(dataSearch);
+  return dataSearch.results;
+}
+//getBySearch("batman")
