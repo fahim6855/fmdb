755c05016b99e373d5d24832a1ca89c7882cdb15
Fahim Afnan <142663426+fahim6855@users.noreply.github.com>
2026-03-07 21:22:31 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-let baseUrl = "";
+let page = 2;
 let apiKey = "47e8febd32a5972b12bef57865cb08a5";
-let streamUrl = "https://streamex.net"
+let streamUrl = "https://streamex.net";
 
 document.addEventListener("alpine:init", () => {
   Alpine.data("movieData", () => ({
@@ -22,7 +22,7 @@ document.addEventListener("alpine:init", () => {
 //getPopularMovies();
 async function getPopularMovies() {
   let resPop = await fetch(
-    `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&page=1`
+    `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&page=${page}`
   );
   let dataPop = await resPop.json();
   return dataPop.results;
@@ -30,7 +30,7 @@ async function getPopularMovies() {
 
 //getByGenre("16")
 async function getByGenre(genreId){
-  let resGenre = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=${Number(genreId)}`);
+  let resGenre = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=${Number(genreId)}&page=${page}`);
   let dataGenre = await resGenre.json();
   return dataGenre.results;
 }
@@ -42,4 +42,5 @@ async function getBySearch(QueryTitle){
   console.log(dataSearch);
   return dataSearch.results;
 }
+
 //getBySearch("batman")
